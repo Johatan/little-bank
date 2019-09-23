8762017e7479449e573f9c28ee8915ca22448386
BrunoCordioli <bcordiolimachado7@gmail.com>
2019-09-23 00:57:04 -0300

    Added reception method

--- little-bank/src/main/java/main/Trabalho1.java
+++ little-bank/src/main/java/main/Trabalho1.java
@@ -1,11 +1,14 @@
 package main;
 
+import java.util.Scanner;
+
 public class Trabalho1 {
 	static String[][] A = new String[3][3];
 	static int[][] B = new int[3][3];
 	static double[][] C = new double[3][3];
+	static Scanner scanner = new Scanner(System.in);
 	
-	public static void setVariables() {
+	public static void setBankData() {
 		A[0][0] = "Tibúrcio";
 		A[0][1] = "Ricardo";
 		A[0][2] = "João";
@@ -14,7 +17,7 @@ public class Trabalho1 {
 		A[1][2] = "Dona Maria";
 		A[2][0] = "Gedonstrôncio";
 		A[2][1] = "Batman";
-		A[2][2] = "Homem deFerro";
+		A[2][2] = "Homem de Ferro";
 		
 		B[0][0] = 111;
 		B[0][1] = 222;
@@ -39,6 +42,39 @@ public class Trabalho1 {
 	
 	
 	public static void main(String args[]){
-		setVariables();
+		run_();
+	}
+	public static void run_() {
+		setBankData();
+		reception();
+	}
+	
+	public static void reception() {
+		System.out.println("Insira o numero da conta na qual você ira tranferir: ");
+		int accountNumber = scanner.nextInt();  
+		System.out.println("Insira o valor na qual será transferido: ");
+		int receivedValor = scanner.nextInt();  
+		
+		boolean flag = false;
+		boolean notFound = false;
+		for (int i = 0; i < B.length; i++) {
+			if (flag == true) {
+				break;
+			}
+			for (int i2 = 0; i2 < B.length; i2++) {
+				if (B[i][i2] == accountNumber) {
+					C[i][i2] += receivedValor;
+					System.out.println(A[i][i2] + " seu saldo atual é de: " + C[i][i2]);
+					flag = true;
+					notFound = false;
+					break;
+				} else {
+					notFound = true;
+				}
+			}
+		}
+		if (notFound == true) {
+			System.out.println("Account NOT FOUND");
+		}
 	}
 }
